e46b7dde509c7d3c18812465ad16ffc62a49efc2
arles1224 <nighthawks1224@icloud.com>
2023-04-14 11:56:46 +0900

    master

--- java2/src/project1/Array2.java
+++ java2/src/project1/Array2.java
@@ -1,10 +1,30 @@
 package project1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Array2 {
-
+	// Object 배열 응용편
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Object 배열에 숫자와 문자 데이터를 각각 추가
+		ArrayList<Object> al = new ArrayList<>();
+		al.add("hong");
+		al.add(25);
+		al.add("hong@nate.com");
+		al.add(6000);
+		al.add("01023451235");
+		System.out.println(al);
+		System.out.println(al.get(3).getClass().getName());
+		
+		// 2차 원시 Object 배열
+		Object data[][] = {
+				{"hong","kim","park","jang"},
+				{25,30,44,21}
+		};
+		System.out.println(Arrays.toString(data[1]));
+		
+		// 복합적 Object 배열
+		Object[] userinfo = {"hong",23,"서울","01023457789",5000};
 	}
 
 }
---
+++ java2/src/project1/Array3.java
@@ -0,0 +1,19 @@
+package project1;
+
+import java.util.ArrayList;
+
+public class Array3 {
+	// 원시 배열 값을 Object 배열로 이관하기
+	public static void main(String[] args) {
+		Ob1 ob1 = new Ob1();
+		ob1.data1();
+	}
+
+}
+class Ob1{
+	ArrayList<Object> al;
+	Object data[][];
+	public void data1() {
+		
+	}
+}
